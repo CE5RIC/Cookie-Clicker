68babac15485a9321b46816becf09f3df99ea3e4
CE5RIC <cedricdierckx5@hotmail.com>
2022-05-17 15:02:55 +0200

    created a multiplier function

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
+
+
+
+
 window.onload = function () {
     let clickDiv = document.getElementById("click-div");
     clickDiv.onclick = incrementClick;
 
     let resetBtn = document.getElementById("reset-button");
     resetBtn.onclick = resetCounter;
+
+    let multiBtn = document.getElementById("multiply-btn");
+    multiBtn.onclick = multiplierClick;
 }
 
+// Counter variable
 var counterVal = 0;
 
-incrementClick = function() {
+// Increment by one
+function incrementClick() {
     updateDisplay(++counterVal);
 }
 
+// multiply by counter by 2
+function multiplierClick() {
+   updateDisplay(counterVal * 2);
+}
+
+// function to reset the counter to 0 and calls updatedisplay function
 function resetCounter() {
     counterVal = 0;
     updateDisplay(counterVal);
 }
+
+
+// TO UPDATE THE COUNTER
 
 function updateDisplay(val) {
     document.getElementById("counter-label").innerHTML = val;
